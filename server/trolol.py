deb93cf6357827165f5b8a1ed099c5a0c49da91a
Shan Shi <sshi@thirtyfourth.(none)>
2014-02-15 15:48:35 -0500

    fuck twilio this shit too hard

--- server/trolol.py
+++ server/trolol.py
@@ -4,13 +4,7 @@ from twilio.rest import TwilioRestClient
 account_sid = "AC30f6fe6eb0d1d674d103005bc1ff37b3"
 auth_token  = "2eb491fe7086e79e7ec3829e346ad029"
 client = TwilioRestClient(account_sid, auth_token)
-
-while(True):
-    from_number = request.values.get('From', None)
-    message = "thanks for the message!"
-    resp = twilio.twiml.Response()
-    resp.message(message)
-
-client.sms.messages.create(body=returnmsg,
-    to="+13473419085",    # Replace with your phone number
-    from_="+19787056029") # Replace with your Twilio number
+for i in range(0,5):
+    client.sms.messages.create(body= str(i)+ "niggers",
+        to="+13473419085",    # Replace with your phone number
+        from_="+19787056029") # Replace with your Twilio number
