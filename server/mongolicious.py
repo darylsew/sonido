abb5ec67d38ba0f18f4f1da4ff9012cd32b56acb
Shan Shi <sshi@thirtyfourth.(none)>
2014-02-15 16:14:18 -0500

    mongoggogogogogogooggoogogogogogogogogogoggoogogog

---
+++ server/mongolicious.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+@app.route('/')
+def home_page():
+    online_users = mongo.db.users.find({'online': True})
+    return render_template('index.html',
+        online_users=online_users)
